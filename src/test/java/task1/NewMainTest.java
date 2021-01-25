86990afefd684b7942701845ad46193a1d2ea947
Misha Pilipenko <33057249+HANHERO@users.noreply.github.com>
2021-01-25 16:19:10 +0200

    комит с ноута

---
+++ src/test/java/task1/NewMainTest.java
@@ -0,0 +1,19 @@
+package task1;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static task1.Main.s;
+
+public class NewMainTest {
+    private final int min = Integer.MIN_VALUE;
+    private final int max = Integer.MAX_VALUE;
+}
+class Expected {
+    int a;
+    int b;
+    Expected(int a, int b){
+        this.a = a;
+        this.b = b;
+    }
+}
