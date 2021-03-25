6d6284479e69fdb959d6611b3a94a0b1b99f7802
Misha <pmixmix3@gmail.com>
2021-03-25 15:15:20 +0200

    added documentation

---
+++ src/main/java/task5/Month.java
@@ -0,0 +1,37 @@
+package task5;
+
+/**
+ * The enum Month.
+ *
+ *
+ * @see Year
+ * @author Michael Pilipenko
+ * @version 1.0
+ *
+ */
+public enum Month {
+    /**January month.*/
+    JANUARY,
+    /**February month.*/
+    FEBRUARY,
+    /**March month.*/
+    MARCH,
+    /**April month.*/
+    APRIL,
+    /**May month.*/
+    MAY,
+    /**June month.*/
+    JUNE,
+    /**July month.*/
+    JULY,
+    /**August month.*/
+    AUGUST,
+    /**September month.*/
+    SEPTEMBER,
+    /**October month.*/
+    OCTOBER,
+    /**November month.*/
+    NOVEMBER,
+    /**December month.*/
+    DECEMBER
+}
