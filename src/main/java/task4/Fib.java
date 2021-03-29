29e5611d991fccccbc862daf7b832d0075ef5702
Misha <pmixmix3@gmail.com>
2021-03-29 11:51:58 +0300

    added documentation

--- src/main/java/task4/Fib.java
+++ src/main/java/task4/Fib.java
@@ -1,8 +1,5 @@
 package task4;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.math.BigInteger;
 import java.util.Scanner;
 
@@ -23,10 +20,6 @@ import java.util.Scanner;
  * @version 1.0
  */
 public class Fib {
-    /**Variables temp, a and b. Used as temporary storage for calculating Fibonacci sequence.*/
-    private static BigInteger a = BigInteger.valueOf(0);
-    private static BigInteger b = BigInteger.valueOf(1);
-
 
     /**
      * The entry point of application. This is where user input is expected and validates.
@@ -59,12 +52,11 @@ public class Fib {
      * @return the big integer.
      */
     public static BigInteger findFib(int n) {
+        BigInteger a = BigInteger.valueOf(0);
+        BigInteger b = BigInteger.valueOf(1);
         int nABS = Math.abs(n);
 
         for (int i = 1; i < nABS; i++) {
-            //temp = b;
-            //b = a.add(b);
-            //a = temp;
             a = a.add(b);
             b = a.subtract(b);
             a = a.subtract(b);
