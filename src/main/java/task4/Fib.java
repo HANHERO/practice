460f43d91017eff511431533bb5b03898a8376fc
Misha <pmixmix3@gmail.com>
2021-03-29 11:52:58 +0300

    added documentation

--- src/main/java/task4/Fib.java
+++ src/main/java/task4/Fib.java
@@ -68,6 +68,7 @@ public class Fib {
         } else if (n < 0 && (n % 2) == 0) { //if the number is negative and even
             b = b.multiply(BigInteger.valueOf(-1));
         }
+
         return b;
     }
 }
